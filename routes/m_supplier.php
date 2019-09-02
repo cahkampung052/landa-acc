67d8eaa48245486e6254ff5a10105b5027bc54c3
miku404 <ggmild55@gmail.com>
2019-09-02 17:34:40 +0700

    commit revisi 31

--- routes/m_supplier.php
+++ routes/m_supplier.php
@@ -16,12 +16,18 @@ $app->get('/acc/m_supplier/kode', function ($request, $response){
 
 $app->get('/acc/m_supplier/getSupplier', function ($request, $response) {
     $db = $this->db;
-    $models = $db->select("*")
+    $params = $request->getParams();
+    $db->select("*")
                 ->from("acc_m_kontak")
                 ->orderBy("acc_m_kontak.nama")
                 ->where("is_deleted", "=", 0)
-                ->where("type", "=", "supplier")
-                ->findAll();
+                ->where("type", "=", "supplier");
+
+                if(isset($params['nama']) && !empty($params['nama'])){
+                    $db->customWhere("nama LIKE '%" . $params['nama'] . "%'", "AND");
+                }
+
+                $models = $db->limit(20)->findAll();
     return successResponse($response, [
       'list' => $models
     ]);
