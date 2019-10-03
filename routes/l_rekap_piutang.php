7337f1bbe20b7c2a3b15f8855cc0b15e8fc03b12
Muhamad Adi Setyawan <ggmild55@gmail.com>
2019-10-03 13:08:36 +0700

    adi

--- routes/l_rekap_piutang.php
+++ routes/l_rekap_piutang.php
@@ -1,8 +1,9 @@
 <?php
+
 $app->get('/acc/l_rekap_piutang/laporan', function ($request, $response) {
     $params = $request->getParams();
     $sql = $this->db;
-    
+
 //    print_r($params);die();
     /**
      * tanggal awal
@@ -29,103 +30,144 @@ $app->get('/acc/l_rekap_piutang/laporan', function ($request, $response) {
     /*
      * Siapkan parameter lokasi
      */
-    if(isset($params['m_lokasi_id'])){
+    if (isset($params['m_lokasi_id'])) {
         $lokasiId = getChildId("acc_m_lokasi", $params['m_lokasi_id']);
         /*
          * jika lokasi punya child
          */
-        if(!empty($lokasiId)){
+        if (!empty($lokasiId)) {
             $lokasiId[] = $params['m_lokasi_id'];
             $lokasiId = implode(",", $lokasiId);
         }
         /*
          * jika lokasi tidak punya child
-         */
-        else{
+         */ else {
             $lokasiId = $params['m_lokasi_id'];
         }
     }
-    
-    
+
+
     /**
      * Proses laporan
      */
     if (isset($params['m_akun_id']) && isset($params['m_lokasi_id'])) {
-        
+
         /*
          * ambil supplier
          */
-        $supplier = $sql->select("*")
-                ->from("acc_m_kontak")
-                ->where("is_deleted", "=", 0)
-                ->where("type", "=", "customer")
-                ->findAll();
-        
+//        $supplier = $sql->select("*")
+//                ->from("acc_m_kontak")
+//                ->where("is_deleted", "=", 0)
+//                ->where("type", "=", "customer")
+//                ->findAll();
+
         $data['totalSaldoAwal'] = 0;
         $data['totalDebit'] = 0;
         $data['totalKredit'] = 0;
         $data['totalSaldoAkhir'] = 0;
-        $arr=[];
-        foreach($supplier as $key => $val){
-            $arr[$key] = (array) $val;
-            /*
-            * ambil saldo awal piutang ke customer
-            */
-           $sql->select('sum(acc_trans_detail.debit) as debit, sum(acc_trans_detail.kredit) as kredit')
-               ->from('acc_trans_detail');
-               if(isset($params['m_lokasi_id']) && !empty($params['m_lokasi_id'])){
-                   $sql->customWhere("acc_trans_detail.m_lokasi_id IN ($lokasiId)");
-               }
-           $sql->andWhere('acc_trans_detail.m_akun_id', '=', $params['m_akun_id'])
-               ->andWhere('acc_trans_detail.m_kontak_id', '=', $val->id)
-               ->andWhere('date(acc_trans_detail.tanggal)', '<', $tanggal_start);
+        $arr = [];
+//        foreach($supplier as $key => $val){
+//            $arr[$key] = (array) $val;
+        /*
+         * ambil saldo awal piutang ke customer
+         */
+        $sql->select('sum(acc_trans_detail.debit) as debit, sum(acc_trans_detail.kredit) as kredit')
+                ->from('acc_trans_detail');
+        if (isset($params['m_lokasi_id']) && !empty($params['m_lokasi_id'])) {
+            $sql->customWhere("acc_trans_detail.m_lokasi_id IN ($lokasiId)");
+        }
+        $sql->andWhere('acc_trans_detail.m_akun_id', '=', $params['m_akun_id'])
+//                ->andWhere('acc_trans_detail.m_kontak_id', '=', $val->id)
+                ->andWhere('date(acc_trans_detail.tanggal)', '<', $tanggal_start);
 
-           $getsaldoawal = $sql->find();
-           $arr[$key]["saldoAwal"] = $getsaldoawal->debit - $getsaldoawal->kredit;
-           
-           /*
-            * ambil saldo hutang di rentan tanggal dari customer
-            */
-           $sql->select('sum(acc_trans_detail.debit) as debit, sum(acc_trans_detail.kredit) as kredit')
-               ->from('acc_trans_detail');
-               if(isset($params['m_lokasi_id']) && !empty($params['m_lokasi_id'])){
-                   $sql->customWhere("acc_trans_detail.m_lokasi_id IN ($lokasiId)");
-               }
-           $sql->andWhere('acc_trans_detail.m_akun_id', '=', $params['m_akun_id'])
-                ->andWhere('acc_trans_detail.m_kontak_id', '=', $val->id)
+        $getsaldoawal = $sql->findAll();
+
+        $arrkontak = [];
+        $arrawal = [];
+        foreach ($getsaldoawal as $key => $val) {
+            if (isset($arrawal[$val->m_kontak_id])) {
+                $arrawal[$val->m_kontak_id]['debit'] += $val->debit;
+                $arrawal[$val->m_kontak_id]['kredit'] += $val->kredit;
+            } else {
+                $arrawal[$val->m_kontak_id] = (array) $val;
+                if (!in_array($val->m_kontak_id, $arrkontak)) {
+                    $arrkontak[] = $val->m_kontak_id;
+                }
+            }
+        }
+
+        /*
+         * ambil saldo hutang di rentan tanggal dari customer
+         */
+        $sql->select('sum(acc_trans_detail.debit) as debit, sum(acc_trans_detail.kredit) as kredit')
+                ->from('acc_trans_detail');
+        if (isset($params['m_lokasi_id']) && !empty($params['m_lokasi_id'])) {
+            $sql->customWhere("acc_trans_detail.m_lokasi_id IN ($lokasiId)");
+        }
+        $sql->andWhere('acc_trans_detail.m_akun_id', '=', $params['m_akun_id'])
+//                ->andWhere('acc_trans_detail.m_kontak_id', '=', $val->id)
                 ->andWhere('date(acc_trans_detail.tanggal)', '>=', $tanggal_start)
                 ->andWhere('date(acc_trans_detail.tanggal)', '<=', $tanggal_end);
 
-           $getsaldopiutang = $sql->find();
-           
-           /*
-            * isi debit kredit di detail
-            */
-           $arr[$key]["debit"] = ($getsaldopiutang->debit != NULL) ? $getsaldopiutang->debit : 0;
-           $arr[$key]["kredit"] = ($getsaldopiutang->kredit != NULL) ? $getsaldopiutang->kredit : 0;
-           
-           /*
-            * hitung saldo akhir detail
-            */
-           $arr[$key]["saldoAkhir"] = $arr[$key]["saldoAwal"] + $arr[$key]["debit"] - $arr[$key]["kredit"];
-           
-           /*
-            * tambahkan detail ke total dari setiap saldo
-            */
-           $data['totalSaldoAwal'] += $arr[$key]["saldoAwal"];
-           $data['totalDebit'] += $arr[$key]["debit"];
-           $data['totalKredit'] += $arr[$key]["kredit"];
-           $data['totalSaldoAkhir'] += $arr[$key]["saldoAkhir"];
-           
+        $getsaldopiutang = $sql->findAll();
+
+        $arrperiode = [];
+        foreach ($getsaldopiutang as $key => $val) {
+            if (isset($arrperiode[$val->m_kontak_id])) {
+                $arrperiode[$val->m_kontak_id]['debit'] += $val->debit;
+                $arrperiode[$val->m_kontak_id]['kredit'] += $val->kredit;
+            } else {
+                $arrperiode[$val->m_kontak_id] = (array) $val;
+                if (!in_array($val->m_kontak_id, $arrkontak)) {
+                    $arrkontak[] = $val->m_kontak_id;
+                }
+            }
         }
-        
-                
+
+        /*
+         * gabungkan keduanya
+         */
+        $data['totalSaldoAwal'] = 0;
+        $data['totalDebit'] = 0;
+        $data['totalKredit'] = 0;
+        $data['totalSaldoAkhir'] = 0;
+        $arr = [];
+        foreach ($arrkontak as $key => $val) {
+            if (isset($arrawal[$val])) {
+                $arr[$val]['saldoAwal'] = $arrawal[$val]['debit'] - $arrawal[$val]['kredit'];
+                $arr[$val]['kode'] = $arrawal[$val]['kode'];
+                $arr[$val]['nama'] = $arrawal[$val]['nama'];
+            } else {
+                $arr[$val]['saldoAwal'] = 0;
+            }
+
+            if (isset($arrperiode[$val])) {
+                $arr[$val]['debit'] = $arrperiode[$val]['debit'];
+                $arr[$val]['kredit'] = $arrperiode[$val]['kredit'];
+                $arr[$val]['kode'] = $arrperiode[$val]['kode'];
+                $arr[$val]['nama'] = $arrperiode[$val]['nama'];
+            } else {
+                $arr[$val]['debit'] = 0;
+                $arr[$val]['kredit'] = 0;
+            }
+
+            $arr[$val]['saldoAkhir'] = $arr[$val]['saldoAwal'] + $arr[$val]['debit'] - $arr[$val]['kredit'];
+
+            $data['totalSaldoAwal'] += $arr[$val]["saldoAwal"];
+            $data['totalDebit'] += $arr[$val]["debit"];
+            $data['totalKredit'] += $arr[$val]["kredit"];
+            $data['totalSaldoAkhir'] += $arr[$val]["saldoAkhir"];
+        }
+
+//        }
+
+
         if (isset($params['export']) && $params['export'] == 1) {
             $view = twigViewPath();
             $content = $view->fetch('laporan/rekapPiutang.html', [
                 "data" => $data,
                 "detail" => $arr,
-                "css" => modulUrl().'/assets/css/style.css',
+                "css" => modulUrl() . '/assets/css/style.css',
             ]);
             header("Content-type: application/vnd.ms-excel");
             header("Content-Disposition: attachment;Filename=laporan-rekap-piutang.xls");
@@ -135,7 +177,7 @@ $app->get('/acc/l_rekap_piutang/laporan', function ($request, $response) {
             $content = $view->fetch('laporan/rekapPiutang.html', [
                 "data" => $data,
                 "detail" => $arr,
-                "css" => modulUrl().'/assets/css/style.css',
+                "css" => modulUrl() . '/assets/css/style.css',
             ]);
             echo $content;
             echo '<script type="text/javascript">window.print();setTimeout(function () { window.close(); }, 500);</script>';
