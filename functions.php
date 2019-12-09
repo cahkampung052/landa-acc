b66fc5ac7ba0af2249c2bf09d372f11408385c71
miku404 <ggmild55@gmail.com>
2019-12-09 09:59:13 +0700

    adi

--- functions.php
+++ functions.php
@@ -686,6 +686,11 @@ function generateNoTransaksi($type, $unker, $preffix = null, $bulan = null, $tah
         $no_transaksi = str_replace("BULAN", $bulan, $no_transaksi);
         $no_transaksi = str_replace("KODEUNIT", $unker, $no_transaksi);
         $no_transaksi = str_replace("NOURUT", $no_urut, $no_transaksi);
+    } elseif ($type == 'pelabuhan') {
+        $cek = $db->find("select kode from acc_m_kontak where type = 'pelabuhan' order by kode desc");
+        $urut = (empty($cek)) ? 1 : ((int) substr($cek->kode, -5)) + 1;
+        $no_urut = substr('00000' . $urut, -5);
+        $no_transaksi = "PLB" . $tahun . "" . $no_urut;
     }
     return $no_transaksi;
 }
