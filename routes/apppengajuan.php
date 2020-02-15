e5716860755ae19de5095a54763aaaf41aeed4b4
miku404 <ggmild55@gmail.com>
2020-02-15 12:22:19 +0700

    adi

--- routes/apppengajuan.php
+++ routes/apppengajuan.php
@@ -486,7 +486,10 @@ $app->get("/acc/apppengajuan/printPengajuan", function ($request, $response) {
     $data = $request->getParams();
     $tableuser = tableUser();
     $db = $this->db;
-    $db->select("*")->from("acc_t_pengajuan_det")->where("t_pengajuan_id", "=", $data['id']);
+    $db->select("acc_t_pengajuan_det.*, acc_m_akun.kode as kodeAkun, acc_m_akun.nama as namaAkun")
+            ->from("acc_t_pengajuan_det")
+            ->join("LEFT JOIN", "acc_m_akun", "acc_m_akun.id = acc_t_pengajuan_id.m_akun_id")
+            ->where("t_pengajuan_id", "=", $data['id']);
     $detail = $db->findAll();
     foreach ($detail as $key => $val) {
         $val->no = $key + 1 . ".";
