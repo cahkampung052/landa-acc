549d95de247aeac121bec00ad99561ab9e063970
miku404 <ggmild55@gmail.com>
2020-10-23 19:54:38 +0700

    adi

--- routes/l_budgeting.php
+++ routes/l_budgeting.php
@@ -1,8 +1,12 @@
 <?php
+
 $app->get('/acc/l_budgeting/laporan', function ($request, $response) {
     $data['img'] = imgLaporan();
     $params = $request->getParams();
     $db = $this->db;
+
+//    print_die($params);
+
     /**
      * Ambil data akun
      */
@@ -34,9 +38,43 @@ $app->get('/acc/l_budgeting/laporan', function ($request, $response) {
     /**
      * Jika akun tidak ditemukan, laporan = kosong
      */
-    if(!isset($getakun->id)){
+    if (!isset($getakun->id)) {
         return successResponse($response, []);
     }
+
+    /*
+     * kelola tanggal
+     */
+    $tanggal_awal = date("Y-m", strtotime($params['startDate'])) . "-01";
+    $tanggal_akhir = date("Y-m", strtotime($params['endDate'])) . "-01";
+    $tanggal_akhir = date("Y-m-d", strtotime($tanggal_akhir . '+1 month'));
+
+//    print_die($tanggal_awal);
+//    print_die($tanggal_akhir);
+
+    $tanggal_sekarang = $tanggal_awal;
+    $arr_tanggal = [];
+    $arr_bulan = [];
+    $arr_tahun = [];
+    do {
+        $arr_tanggal[] = [
+            'awal' => date("Y-m-d", strtotime($tanggal_sekarang)),
+            'akhir' => date("Y-m-t", strtotime($tanggal_sekarang)),
+            'nama' => date("F", strtotime($tanggal_sekarang)),
+        ];
+
+        $arr_bulan[] = date("m", strtotime($tanggal_sekarang)) < 10 ? '0' . date("m", strtotime($tanggal_sekarang)) : date("m", strtotime($tanggal_sekarang));
+        $arr_tahun[] = date("Y", strtotime($tanggal_sekarang));
+
+        $tanggal_sekarang = date("Y-m-d", strtotime($tanggal_sekarang . '+1 month'));
+    } while ($tanggal_sekarang != $tanggal_akhir);
+
+//    print_die($arr_tanggal);
+//    print_die($arr_bulan);
+//    print_die($arr_tahun);
+    $arr_bulan = implode(", ", array_unique($arr_bulan));
+    $arr_tahun = implode(", ", array_unique($arr_tahun));
+
     /**
      * Ambil id akun turunan klasifikasi di parameter
      */
@@ -57,90 +95,99 @@ $app->get('/acc/l_budgeting/laporan', function ($request, $response) {
             ->where("acc_m_akun.is_deleted", "=", 0)
             ->where("acc_m_akun.is_tipe", "=", 0)
             ->findAll();
-    $list = [];
-    foreach ($listAkun as $key => $value) {
-        $listAkun[$key] = (array) $value;
-        /*
-         * perulangan sebanyak 12 (bulan)
-         */
-        for ($i = 1; $i <= 12; $i++) {
-            /*
-             * ambil budget
-             */
-            $db->select("*")->from("acc_budgeting")
-                    ->where("m_akun_id", "=", $value->id)
-                    ->where("tahun", "=", $params['tahun'])
-                    ->where("bulan", "=", $i);
-            if (!empty($lokasiId)) {
-                $db->customWhere("acc_budgeting.m_lokasi_id = '".$lokasiId."'", "AND");
-            }
-            $getBudget = $db->find();
-            /*
-             * set nomer bulan
-             */
-            $bulan = $i;
-            if ($i < 10) {
-                $bulan = 0 . "" . $i;
-            }
-            /*
-             * ambil transdetail
-             */
-            $db->select("SUM(debit-kredit) AS nominal")->from("acc_trans_detail");
-            if (!empty($lokasiId)) {
-                $db->customWhere("acc_trans_detail.m_lokasi_id IN($lokasiId)", "AND");
-            }
-            $db->where("m_akun_id", "=", $value->id)
-                    ->where("tanggal", "LIKE", "" . $params['tahun'] . "-" . $bulan . "");
-            $getTransDetail = $db->find();
-            /*
-             * isi nominal target dari budget
-             */
-            if (!$getBudget) {
-                $listAkun[$key]['detail'][$i]['target'] = 0;
-            } else {
-                $listAkun[$key]['detail'][$i]['target'] = $getBudget->budget;
-            }
-            /*
-             * isi nominal realisasi dari transdetail
-             */
-            if (!$getTransDetail || $getTransDetail->nominal == null) {
-                $listAkun[$key]['detail'][$i]['realisasi'] = 0;
-            } else {
-                $listAkun[$key]['detail'][$i]['realisasi'] = $getTransDetail->nominal;
-            }
-            $listAkun[$key]['detail'][$i]['bulan'] = date('F', mktime(0, 0, 0, $i, 10));
+
+//    print_die($listAkun);
+
+    /*
+     * ambil semua budgeting
+     */
+    $db->select("*")->from("acc_budgeting")
+            ->customWhere("m_akun_id in (" . $childId . ")", "AND")
+            ->customWhere("tahun in ($arr_tahun)", "AND")
+            ->customWhere("bulan in ($arr_bulan)", "AND");
+    if (!empty($lokasiId)) {
+        $db->customWhere("acc_budgeting.m_lokasi_id = '" . $lokasiId . "'", "AND");
+    }
+    $getBudget = $db->findAll();
+
+//    print_die($getBudget);
+
+    $arrBudget = [];
+    foreach ($getBudget as $key => $value) {
+        $value->bulan = $value->bulan < 10 ? '0' . $value->bulan : $value->bulan;
+        $arrBudget[$value->m_akun_id]['detail'][$value->tahun . "-" . $value->bulan] = (array) $value;
+    }
+
+//    print_die($arrBudget);
+
+    /*
+     * ambil transdetail
+     */
+    $db->select("*, debit-kredit AS nominal")->from("acc_trans_detail");
+    if (!empty($lokasiId)) {
+        $db->customWhere("acc_trans_detail.m_lokasi_id IN($lokasiId)", "AND");
+    }
+    $db->customWhere("m_akun_id IN($childId)", "AND")
+            ->where("tanggal", ">=", $tanggal_awal)
+            ->where("tanggal", ">=", date("Y-m-d", strtotime($tanggal_akhir . '-1 days')));
+    $getTransDetail = $db->findAll();
+
+//    print_die($getTransDetail);
+
+    $arrTransDetail = [];
+    foreach ($getTransDetail as $key => $value) {
+        if (isset($arrTransDetail[$value->m_akun_id]['detail'][date('Y-m', strtotime($value->tanggal))])) {
+            $arrTransDetail[$value->m_akun_id]['detail'][date('Y-m', strtotime($value->tanggal))]['nominal'] += $value->nominal;
+        } else {
+            $arrTransDetail[$value->m_akun_id]['detail'][date('Y-m', strtotime($value->tanggal))] = (array) $value;
         }
     }
-    /*
-     * perulangan bulan untuk header
-     */
-    $listBulan = [];
-    for ($i = 1; $i <= 12; $i++) {
-        $listBulan[$i]['bulan'] = date('F', mktime(0, 0, 0, $i, 10));
+
+//    print_die($arrTransDetail);
+
+    $list = [];
+    $arr = [];
+    foreach ($listAkun as $key => $value) {
+        $listAkun[$key] = (array) $value;
+
+        $arr[$value->id] = (array) $value;
+        foreach ($arr_tanggal as $k => $v) {
+            $arr[$value->id]['detail'][date("Y-m", strtotime($v['awal']))]['budget'] = isset($arrBudget[$value->id]['detail'][date("Y-m", strtotime($v['awal']))]['budget']) ? $arrBudget[$value->id]['detail'][date("Y-m", strtotime($v['awal']))]['budget'] : 0;
+            $arr[$value->id]['detail'][date("Y-m", strtotime($v['awal']))]['realisasi'] = isset($arrTransDetail[$value->id]['detail'][date("Y-m", strtotime($v['awal']))]['nominal']) ? $arrTransDetail[$value->id]['detail'][date("Y-m", strtotime($v['awal']))]['nominal'] : 0;
+            if (!isset($arr[$value->id]['total'])) {
+                $arr[$value->id]['total'] = $arr[$value->id]['detail'][date("Y-m", strtotime($v['awal']))]['budget'];
+            } else {
+                $arr[$value->id]['total'] += $arr[$value->id]['detail'][date("Y-m", strtotime($v['awal']))]['budget'];
+            }
+            $arr[$value->id]['total'] += $arr[$value->id]['detail'][date("Y-m", strtotime($v['awal']))]['realisasi'];
+        }
     }
-    /*
-     * perulangan setiap bulan 2 nominal
-     */
-    $listSetiapBulan = [];
-    $a = 1;
-    for ($i = 1; $i <= 12; $i++) {
-        $listSetiapBulan[$a]['nama'] = 'Target';
-        $listSetiapBulan[$a + 1]['nama'] = 'Realisasi';
-        $a += 2;
+
+//    print_die($arr);
+
+    foreach ($arr as $key => $value) {
+        if ($value['total'] == 0) {
+            unset($arr[$key]);
+        }
     }
+
+//    print_die($arr);
+
     /*
      * return
      */
-    $data['bulan'] = $listBulan;
-    $data['setiapbulan'] = $listSetiapBulan;
+    $data['bulan'] = $arr_tanggal;
+    $data['jumlah_bulan'] = count($arr_tanggal);
+    $data['tanggal'] = date("d M Y", strtotime($params['startDate'])) . ' s/d ' . date("d M Y", strtotime($params['endDate']));
+//    $data['setiapbulan'] = $listSetiapBulan;
     $data['disiapkan'] = date("d-m-Y, H:i");
     $data['lokasi'] = $params['nama_lokasi'];
-    $data['tanggal'] = $params['tahun'];
+//    $data['tanggal'] = $params['tahun'];
     if (isset($params['export']) && $params['export'] == 1) {
         $view = twigViewPath();
         $content = $view->fetch('laporan/budgeting.html', [
             "data" => $data,
-            "detail" => $listAkun,
+            "detail" => $arr,
             "css" => modulUrl() . '/assets/css/style.css',
         ]);
         header("Content-type: application/vnd.ms-excel");
@@ -150,12 +197,12 @@ $app->get('/acc/l_budgeting/laporan', function ($request, $response) {
         $view = twigViewPath();
         $content = $view->fetch('laporan/budgeting.html', [
             "data" => $data,
-            "detail" => $listAkun,
+            "detail" => $arr,
             "css" => modulUrl() . '/assets/css/style.css',
         ]);
         echo $content;
         echo '<script type="text/javascript">window.print();setTimeout(function () { window.close(); }, 500);</script>';
     } else {
-        return successResponse($response, ['detail' => $listAkun, 'data' => $data]);
+        return successResponse($response, ['detail' => $arr, 'data' => $data]);
     }
 });
--- tpl/l_budgeting/index.js
+++ tpl/l_budgeting/index.js
@@ -1,8 +1,11 @@
-app.controller('l_budgetingCtrl', function($scope, Data, $rootScope, $uibModal) {
+app.controller('l_budgetingCtrl', function ($scope, Data, $rootScope, $uibModal) {
     var control_link = "acc/l_budgeting";
     $scope.form = {};
-    $scope.form.tahun = new Date();
-    Data.get('acc/m_lokasi/getLokasi').then(function(response) {
+    $scope.form.tanggal = {
+        endDate: moment().add(1, 'M'),
+        startDate: moment()
+    }
+    Data.get('acc/m_lokasi/getLokasi').then(function (response) {
         $scope.listLokasi = response.data.list;
         if ($scope.listLokasi.length > 0) {
             $scope.form.m_lokasi_id = $scope.listLokasi[0];
@@ -11,7 +14,7 @@ app.controller('l_budgetingCtrl', function($scope, Data, $rootScope, $uibModal) 
     /**
      * Ambil list semua akun
      */
-    Data.get('acc/m_akun/akunBebanPendapatan').then(function(data) {
+    Data.get('acc/m_akun/akunBebanPendapatan').then(function (data) {
         $scope.listAkun = data.data.list;
         if ($scope.listAkun.length > 0) {
             $scope.form.m_akun_id = $scope.listAkun[0];
@@ -20,17 +23,18 @@ app.controller('l_budgetingCtrl', function($scope, Data, $rootScope, $uibModal) 
     /**
      * Ambil laporan dari server
      */
-    $scope.view = function(is_export, is_print) {
+    $scope.view = function (is_export, is_print) {
         var param = {
             export: is_export,
             print: is_print,
             m_akun_id: $scope.form.m_akun_id.id,
             m_lokasi_id: $scope.form.m_lokasi_id.id,
             nama_lokasi: $scope.form.m_lokasi_id.nama,
-            tahun: moment($scope.form.tahun).format('YYYY')
+            startDate: moment($scope.form.tanggal.startDate).format('YYYY-MM-DD'),
+            endDate: moment($scope.form.tanggal.endDate).format('YYYY-MM-DD'),
         };
         if (is_export == 0 && is_print == 0) {
-            Data.get(control_link + '/laporan', param).then(function(response) {
+            Data.get(control_link + '/laporan', param).then(function (response) {
                 if (response.status_code == 200) {
                     $scope.data = response.data.data;
                     $scope.detail = response.data.detail;
@@ -41,7 +45,7 @@ app.controller('l_budgetingCtrl', function($scope, Data, $rootScope, $uibModal) 
                 }
             });
         } else {
-            Data.get('site/base_url').then(function(response) {
+            Data.get('site/base_url').then(function (response) {
                 window.open(response.data.base_url + "api/acc/l_budgeting/laporan?" + $.param(param), "_blank");
             });
         }
